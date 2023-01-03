e5c7a941092b746015d8f38c1bc026683e6569ef
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-01-03 10:28:07 +0100

    Add user to model

--- travels_app/my_travels/models.py
+++ travels_app/my_travels/models.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.contrib.gis.db import models
+
+User= settings.AUTH_USER_MODEL
 
 class PlacesVisited(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
     visit_date = models.DateField()
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000, blank=True)
     photo = models.FileField(upload_to='travel_photos', blank=True)
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
 
