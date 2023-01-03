1f4974d802a803f43dcffbf6971490b016051c2a
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-01-03 17:56:31 +0100

    Change map on edit page to osm

--- travels_app/my_travels/forms.py
+++ travels_app/my_travels/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.contrib.gis import forms
 from .models import PlacesVisited
 
 class PlaceForm(forms.ModelForm):
-
+    location = forms.PointField(widget=
+        forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500}))
     class Meta:
         model = PlacesVisited
         fields = ("name", "visit_date", "description", "location")
