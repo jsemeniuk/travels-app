337b13fda98b5456805e07fe4a5e0d5ed04a6f49
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-01-03 16:43:47 +0100

    Add main page redirect to page title

--- travels_app/my_travels/urls.py
+++ travels_app/my_travels/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("map/", views.TravelsMapView.as_view()),
+    path("map/", views.TravelsMapView.as_view(), name='main_page'),
     path('map/<int:pk>/', views.place_detail, name='place_detail'),
     path('map/<int:pk>/edit/', views.place_edit, name='place_edit'),
 ]
