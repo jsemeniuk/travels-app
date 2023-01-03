c6885895a0115b68d59478791c4275bf3c13f699
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-01-03 16:33:42 +0100

    Change type of description field

--- travels_app/my_travels/models.py
+++ travels_app/my_travels/models.py
@@ -7,7 +7,7 @@ class PlacesVisited(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
     visit_date = models.DateField()
-    description = models.CharField(max_length=1000, blank=True)
+    description = models.TextField()
     photo = models.FileField(upload_to='travel_photos', blank=True)
     user= models.ForeignKey(User, on_delete=models.CASCADE)
 
