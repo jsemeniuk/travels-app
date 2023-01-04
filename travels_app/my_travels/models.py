cec659525f853b3a702c6ceed93ce6f92ee04eec
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-01-04 17:37:25 +0100

    Change visit date and description fields to be optional

--- travels_app/my_travels/models.py
+++ travels_app/my_travels/models.py
@@ -6,8 +6,8 @@ User= settings.AUTH_USER_MODEL
 class PlacesVisited(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
-    visit_date = models.DateField()
-    description = models.TextField()
+    visit_date = models.DateField(blank=True)
+    description = models.TextField(blank=True)
     photo = models.FileField(upload_to='travel_photos', blank=True)
     user= models.ForeignKey(User, on_delete=models.CASCADE)
 
