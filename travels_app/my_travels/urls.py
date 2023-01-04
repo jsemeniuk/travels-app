4ed36945969277557ac98c98d626ff94c17bbcdf
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-01-04 14:23:51 +0100

    Update urls for map to be shown on main page

--- travels_app/my_travels/urls.py
+++ travels_app/my_travels/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("map/", views.TravelsMapView.as_view(), name='main_page'),
-    path('map/<int:pk>/', views.place_detail, name='place_detail'),
-    path('map/<int:pk>/edit/', views.place_edit, name='place_edit'),
-    path('map/new', views.place_new, name='place_new'),
+    path("", views.TravelsMapView.as_view(), name='main_page'),
+    path('<int:pk>/', views.place_detail, name='place_detail'),
+    path('<int:pk>/edit/', views.place_edit, name='place_edit'),
+    path('new', views.place_new, name='place_new'),
 ]
