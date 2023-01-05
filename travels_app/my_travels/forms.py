60796ed8d28048923cabcb11a4789b428afe9da4
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-01-05 15:58:45 +0100

    Add default to date in form and adjust base page

--- travels_app/my_travels/forms.py
+++ travels_app/my_travels/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from django.contrib.gis import forms
 from .models import PlacesVisited
+from datetime import datetime
+from django.contrib.admin import widgets 
 
 class PlaceForm(forms.ModelForm):
     location = forms.PointField(widget=
         forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500}))
+    visit_date = forms.DateField(
+            widget=widgets.AdminDateWidget(format='%d.%m.%Y'),
+            input_formats=['%d.%m.%Y'],
+            initial=datetime.today()
+            )
     class Meta:
         model = PlacesVisited
         fields = ("name", "visit_date", "description", "location")
