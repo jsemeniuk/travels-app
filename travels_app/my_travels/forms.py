13064d13fc515d47f57a3ce53bb0de05d960aa3c
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-02-26 15:35:46 +0100

    Remove code duplication for forms

--- travels_app/my_travels/forms.py
+++ travels_app/my_travels/forms.py
@@ -4,25 +4,22 @@ from .models import Places
 from datetime import datetime
 from django.contrib.admin import widgets 
 
-class NewPlaceForm(forms.ModelForm):
+class PlaceForm(forms.ModelForm):
     visit_date = forms.DateField(
+            label='Date',
             widget=widgets.AdminDateWidget(format='%d.%m.%Y'),
             input_formats=['%d.%m.%Y'],
             initial=datetime.today()
             )
+
+class NewPlaceForm(PlaceForm):
     class Meta:
         model = Places
         fields = ("name", "visit_date", "description", "group")
 
-class EditPlaceForm(forms.ModelForm):
+class EditPlaceForm(PlaceForm):
     location = forms.PointField(widget=
-        forms.OSMWidget(attrs={'map_width': 800, 
-                                'map_height': 500}))
-    visit_date = forms.DateField(
-            widget=widgets.AdminDateWidget(format='%d.%m.%Y'),
-            input_formats=['%d.%m.%Y'],
-            initial=datetime.today()
-            )
+        forms.OSMWidget(attrs={'map_height': 500}))
     class Meta:
         model = Places
-        fields = ("name", "visit_date", "description", "location", "group")
+        fields = ("name", "visit_date", "description", "group", "location")
