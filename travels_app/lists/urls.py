fa1159145602672fe4051354609070e99848734a
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-04-12 19:19:28 +0200

    Add editing list items option

--- travels_app/lists/urls.py
+++ travels_app/lists/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('lists', views.lists_all, name='lists_all'),
     path('list/new', views.add_list, name='list_new'),
     path('list/<int:pk>', views.add_items, name='list_edit'),
+    path('list/<int:pk>/<int:item_to_edit>', views.add_items, name='list_edit_item'),
     path('<int:place_id>/list/new', views.add_list, name='list_place_new'),
 ]
--- travels_app/lists/views.py
+++ travels_app/lists/views.py
@@ -22,10 +22,30 @@ def add_list(request, **kwargs):
     return render(request, 'lists/list_new.html', {'form': form})
 
 @login_required
-def add_items(request, pk):
+def add_items(request, pk, **kwargs):
     list_to_edit = get_object_or_404(Lists, pk=pk)
     list_items = Items.objects.filter(list_id=list_to_edit)
-    if request.method == "POST": 
+    if 'item_to_edit' in kwargs:
+        item_to_edit = get_object_or_404(Items, pk=kwargs['item_to_edit'])
+        if request.method == "POST": 
+            form_edit = ManageItems(request.POST, instance=item_to_edit)
+            if form_edit.is_valid():
+                item_to_edit = form_edit.save(commit=False)
+                item_to_edit.save()
+                return redirect("list_edit", pk=pk)
+
+            form = ManageItems(request.POST)
+            if form.is_valid():
+                item = form.save(commit=False)
+                item.list_id = list_to_edit
+                item.save()
+                return redirect("list_edit", pk=pk)
+        else:
+            form_edit = ManageItems(instance=item_to_edit)
+            form = ManageItems()
+        return render(request, 'lists/list_edit.html', {'list': list_to_edit, 'form': form, 'items': list_items, 'form_edit': form_edit, 'edit_item': item_to_edit})
+
+    if request.method == "POST":
         form = ManageItems(request.POST)
         if form.is_valid():
             item = form.save(commit=False)
