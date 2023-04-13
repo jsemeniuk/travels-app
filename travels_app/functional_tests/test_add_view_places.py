d3ecee114031c4fa06caab6ef996b758a683188c
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-04-13 19:35:11 +0200

    Refactor tests for login and Places

--- travels_app/functional_tests/test_add_view_places.py
+++ travels_app/functional_tests/test_add_view_places.py
@@ -7,25 +7,13 @@ import string
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
+from .setup import add_new_place
+
 class AddEditPlaceTest(FunctionalTest):
 #TODO write helpers function to make code clearer (i.e type function)
-#TODO move actions repeatable between test to separate function 
-#TODO create test setup for each test: add new user and new place for user
-    def test_add_place(self):
-        self.browser = webdriver.Chrome()
-        self.browser.get('http://localhost:8000')
-        log_in = self.browser.find_element_by_css_selector('li a[href*=login]')
-        log_in.click()
-        username = self.browser.find_element_by_id('id_username')
-        username.send_keys('TestUser')
-        username = self.browser.find_element_by_id('id_password')
-        username.send_keys('T3stP4ss23')
-        login_button = self.browser.find_element_by_css_selector('input[value=login]')
-        login_button.click()
 
-        self.browser.find_element_by_id('map')
-        #TODO add better way to wait for map to load
-        sleep(5)
+    def test_add_place(self):
+        self.log_in(self.username, self.password)
  
         self.browser.get('http://localhost:8000/new/location=51.00,17.00')
 
@@ -42,20 +30,9 @@ class AddEditPlaceTest(FunctionalTest):
         self.assertEqual(place_title.text, 'Test Place')
 
     def test_view_place(self):
-        self.browser = webdriver.Chrome()
-        self.browser.get('http://localhost:8000')
-        log_in = self.browser.find_element_by_css_selector('li a[href*=login]')
-        log_in.click()
-        username = self.browser.find_element_by_id('id_username')
-        username.send_keys('TestUser')
-        username = self.browser.find_element_by_id('id_password')
-        username.send_keys('T3stP4ss23')
-        login_button = self.browser.find_element_by_css_selector('input[value=login]')
-        login_button.click()
-
-        self.browser.find_element_by_id('map')
-        #TODO add better way to wait for map to load
-        sleep(5)
+        place_id = add_new_place(self.user)
+        
+        self.log_in(self.username, self.password)
  
         place = self.browser.find_element_by_css_selector('#map .leaflet-marker-pane img:first-child')
         place.click()
@@ -63,35 +40,26 @@ class AddEditPlaceTest(FunctionalTest):
         popup_link.click()
 
         place_title = self.browser.find_element_by_css_selector('.header h2')
-        self.assertEqual(place_title.text, 'Borowa')
+        self.assertEqual(place_title.text, 'Test Place')
 
     def test_edit_place(self):
-        self.browser = webdriver.Chrome()
-        self.browser.get('http://localhost:8000')
-        log_in = self.browser.find_element_by_css_selector('li a[href*=login]')
-        log_in.click()
-        username = self.browser.find_element_by_id('id_username')
-        username.send_keys('TestUser')
-        username = self.browser.find_element_by_id('id_password')
-        username.send_keys('T3stP4ss23')
-        login_button = self.browser.find_element_by_css_selector('input[value=login]')
-        login_button.click()
+        place_id = add_new_place(self.user)
 
-        self.browser.get('http://localhost:8000/18/')
+        self.log_in(self.username, self.password)
+
+        self.browser.get(f'http://localhost:8000/{place_id}/')
 
         edit_button = self.browser.find_element_by_css_selector('.glyphicon-pencil')
         edit_button.click()
 
         new_name = ''.join(random.choices(string.ascii_uppercase, k=10))
         name = self.browser.find_element_by_id('id_name')
-        name.send_keys(Keys.CONTROL + "a")
-        name.send_keys(Keys.DELETE)
+        name.clear()
         name.send_keys(new_name)
 
         new_description = ''.join(random.choices(string.ascii_uppercase, k=20))
         description = self.browser.find_element_by_id('id_description')
-        description.send_keys(Keys.CONTROL + "a")
-        description.send_keys(Keys.DELETE)
+        description.clear()
         description.send_keys(new_description)
 
         save_button = self.browser.find_element_by_css_selector('.save')
--- travels_app/functional_tests/test_login_logout.py
+++ travels_app/functional_tests/test_login_logout.py
@@ -4,19 +4,7 @@ from selenium import webdriver
 class LoginLogoutTest(FunctionalTest):
 
     def test_login_logout(self):
-        self.browser = webdriver.Chrome()
-        self.browser.get('http://localhost:8000')
-        log_in = self.browser.find_element_by_css_selector('li a[href*=login]')
-        log_in.click()
-        # TODO add new user as setup
-        username = self.browser.find_element_by_id('id_username')
-        username.send_keys('TestUser')
-        username = self.browser.find_element_by_id('id_password')
-        username.send_keys('T3stP4ss23')
-        login_button = self.browser.find_element_by_css_selector('input[value=login]')
-        login_button.click()
-
-        self.browser.find_element_by_id('map')
+        self.log_in(self.username, self.password)
 
         log_out = self.browser.find_element_by_css_selector('a[href*=logout]')
         log_out.click()
