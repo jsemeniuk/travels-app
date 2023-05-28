d2d5537a7ce0282b93f2e58e00e3ddd503d62fbe
Joanna Semeniuk <semeniuk.asia@gmail.com>
2023-05-28 15:14:34 +0200

    Adjust trip plan app

--- travels_app/trip_plans/forms.py
+++ travels_app/trip_plans/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from .models import TripPlan
 from django.contrib.admin import widgets 
 from datetime import datetime, timedelta
+from my_travels.models import Places
 
 class TripPlanForm(forms.ModelForm):
+
     start_date = forms.DateField(
             label='Start Date',
             widget=widgets.AdminDateWidget(format='%d.%m.%Y'),
@@ -27,7 +29,12 @@ class AddNewPlanForm(TripPlanForm):
 
 
 class EditPlanForm(TripPlanForm):
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['places'].queryset = Places.objects.filter(user=user).filter(group="WI")
 
     class Meta:
         model = TripPlan
-        fields = ("name", "start_date", "end_date", "trip_details", "journey_details")
+        fields = ("name", "start_date", "end_date", "details", "places")
--- travels_app/trip_plans/views.py
+++ travels_app/trip_plans/views.py
@@ -26,14 +26,14 @@ def trip_details(request, pk):
 def trip_plan_edit(request, pk):
     trip = get_object_or_404(TripPlan, pk=pk)
     if request.method == "POST":
-        form = EditPlanForm(request.POST, instance=trip)
+        form = EditPlanForm(request.POST, instance=trip, user=request.user)
         if form.is_valid():
             trip = form.save()
             trip.user = request.user
             trip.save()
             return redirect("trip_details", pk=trip.pk)
     else:
-        form = EditPlanForm(instance=trip)
+        form = EditPlanForm(instance=trip, user=request.user)
     return render(request, 'trip_plans/trip_plan_edit.html', {'form': form, 'trip': trip})
 
 
@@ -43,3 +43,12 @@ def trips_all(request):
     if len(trips_all) == 0:
         trips_all = False
     return render(request, 'trip_plans/trips_all.html', {'trips': trips_all})
+
+@login_required
+def trip_check_list_exists(request, pk):
+    trip = get_object_or_404(TripPlan, pk=pk)
+    if trip.Lists:
+        plan_list = trip.lists
+        return redirect("list_edit", pk=plan_list)
+    else:
+        return redirect("lists_all")  
